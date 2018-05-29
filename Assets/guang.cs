b8bcdc784cfff0feaf56d0f9af6e4eb2ec189fbe
SO4P <37209772+SO4P@users.noreply.github.com>
2018-05-29 20:07:57 +0800

    Add files via upload

---
+++ Assets/guang.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guang : MonoBehaviour {
+
+    public ParticleSystem guangqiu;
+    public ParticleSystem guangyun;
+    public ParticleSystem xingguang;
+    bool co;
+
+    // Use this for initialization
+    void Start () {
+        co = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(200, 200, 80, 80), "change")){
+            xingguang.startSize = (xingguang.startSize + 1) % 3;
+            guangqiu.startColor = (co) ? Color.blue : Color.yellow;
+            guangyun.startColor = (co) ? Color.yellow : Color.blue;
+            co = !co;
+        }
+    }
+}
